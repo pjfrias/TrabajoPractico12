8997db04685edf0760fa7e2eebc0989d859709f8
pjfrias <pjfrias@gmail.com>
2024-05-10 16:47:46 -0300

    Terminado

--- src/construirSA/ConstruirSA.java
+++ src/construirSA/ConstruirSA.java
@@ -12,15 +12,29 @@ import javax.swing.JOptionPane;
 import org.ini4j.Ini;
 
 public class ConstruirSA {
-
+    static Connection conexion = null;
+    
     public static void main(String[] args) {
+        
+        conexion("./conexion.ini");
+        
+        ingresarEmpleados();
+        
+        ingresarHerramientas();
+        
+        listarHerramientas();
+        
+        bajaEmpleado();
+    }
+    
+    public static void conexion(String archivoIni){
         String driver="";
         String db="";
         String user="";
         String pass="";
         //Abrimos conexion.ini para leer los datos de la conexion
         try {
-            File archivo = new File("./conexion.ini");
+            File archivo = new File(archivoIni);
             Ini ini = new Ini(archivo);
             // Leer una sección y una clave
             driver = ini.get("conexion", "driver");
@@ -34,7 +48,7 @@ public class ConstruirSA {
        
         //System.out.println("Driver = "+driver+" Base de datos = "+db+" Usuario = "+user+" Password = "+pass);
         
-        Connection conexion = null;
+        
         
         try {
             Class.forName(driver);
@@ -59,9 +73,11 @@ public class ConstruirSA {
             ex.printStackTrace();
             System.out.println("codigo de error "+ex.getErrorCode());
         }
-        
+    }
+    
+    public static void ingresarEmpleados(){
         try {
-            //Insertamos los 3 empleados
+
             String sql="INSERT INTO empleado(dni, nombre, apellido, acceso, activo) "
                     + "VALUES (34876444,'Nestor','Becerra',1,1),"
                     + "(24877444,'Mario','Kempes',2,0),"
@@ -70,11 +86,67 @@ public class ConstruirSA {
              PreparedStatement ps=conexion.prepareStatement(sql);
              int filas=ps.executeUpdate();
              
-             if(filas >0){
-                JOptionPane.showMessageDialog(null, "Empleado Agregado");
+             if(filas == 3){
+                JOptionPane.showMessageDialog(null, "Empleados Agregados");
              }
-        } catch (Exception e) {
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error ");
+            e.printStackTrace();
         }
-        
+    }
+    
+    public static void ingresarHerramientas(){
+        try {
+  
+            String sql="INSERT INTO herramienta(nombre, descripcion, stock, estado) "
+                    + "VALUES ('Malacate','Hata 5 toneladas',4,1),"
+                    + "('Prensa','Prensa de mesa hidraulica 2 tons',1,0);";
+            
+             PreparedStatement ps=conexion.prepareStatement(sql);
+             int filas=ps.executeUpdate();
+             
+             if(filas == 2){
+                JOptionPane.showMessageDialog(null, "Herrramientas Agregadas");
+             }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error ");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void listarHerramientas(){
+        try {
+          
+            String sql="select nombre, descripcion from herramienta where stock > 4;";
+            
+             PreparedStatement ps=conexion.prepareStatement(sql);
+             ResultSet filas=ps.executeQuery();
+             
+             System.out.println("\n======== Herramientas con stock mayor a 10 unidades =======");
+             while(filas.next()){
+                 System.out.println(filas.getString("nombre")+" -- "+filas.getString("descripcion"));
+             }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error ");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void bajaEmpleado(){
+        try {
+
+            String sql="update empleado set activo = false where id_empleado = 1;";
+            
+             PreparedStatement ps=conexion.prepareStatement(sql);
+             int result=ps.executeUpdate();
+             
+             if(result == 1){
+                 System.out.println("\nEl empleado ahora esta inactivo");
+             }
+                 
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error ");
+            e.printStackTrace();
+        }
     }
 }
