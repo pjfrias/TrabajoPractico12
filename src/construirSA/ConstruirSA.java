e64597bae71861ab453840fd5bf14833ee5cd469
Kyrre1356 <jorge1356@hotmail.com>
2024-05-14 17:13:14 -0300

    Aclaracion Iini

--- src/construirSA/ConstruirSA.java
+++ src/construirSA/ConstruirSA.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package construirSA;
  
 import java.io.File;
 import java.sql.*;
 import javax.swing.JOptionPane;
+// para el correcto funcionamiento de la siguiente linea se debe bajar la libreria desde https://ini4j.sourceforge.net/
 import org.ini4j.Ini;
 
 public class ConstruirSA {
